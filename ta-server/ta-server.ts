d4a7d3a53f754ed47a9b19ed84b41df394717b4c
pauloborba <phmb@cin.ufpe.br>
2019-09-28 18:55:46 -0300

    Ajustando Versão Inicial

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,7 +1,12 @@
 import express = require('express');
+import bodyParser = require("body-parser");
+
+import {Aluno} from '../common/aluno';
+import {CadastroDeAlunos} from './cadastrodealunos';
 
 var taserver = express();
 
+<<<<<<< HEAD
 var alunos = [{nome:'Paulo',
               cpf:'683',
               email:'phmb@cin.br',
@@ -10,11 +15,42 @@ var alunos = [{nome:'Paulo',
               cpf:'456',
               email:'@mcb@cin.br',
               metas:{'requisitos':'MPA'}}];
+=======
+var alunos: CadastroDeAlunos = new CadastroDeAlunos();
 
-taserver.get('/', function (req, res) {
-  res.send(alunos);
+taserver.use(bodyParser.json());
+>>>>>>> e54eeca (versao inicial do servidor, apos extracao de aluno para area comum)
+
+taserver.get('/alunos', function (req, res) {
+  var aluno: string = JSON.stringify(alunos.getAlunos());
+  res.send(aluno);
+})
+
+<<<<<<< HEAD
+taserver.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+=======
+taserver.post('/aluno', function (req: express.Request, res: express.Response) {
+  var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
+  aluno = alunos.criar(aluno);
+  if (aluno) {
+    res.send({"success": "O aluno foi cadastrado com sucesso"});
+  } else {
+    res.send({"failure": "O aluno não pode ser cadastrado"});
+  }
+})
+
+taserver.put('/aluno', function (req: express.Request, res: express.Response) {
+  var aluno: Aluno = <Aluno> req.body;
+  aluno = alunos.atualizar(aluno);
+  if (aluno) {
+    res.send({"success": "O aluno foi atualizado com sucesso"});
+  } else {
+    res.send({"failure": "O aluno não pode ser atualizado"});
+  }
 })
 
 taserver.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!')
+>>>>>>> e54eeca (versao inicial do servidor, apos extracao de aluno para area comum)
 })
