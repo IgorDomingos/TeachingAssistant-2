33e5d7795b686ca4fc574218f59b286c4a62fbb7
IgorDomingos <idrs@cin.ufpe.br>
2021-07-19 22:31:42 -0300

    Não deixar cadastrar alunos com mesmo cpf

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -1,10 +1,12 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
+import { kStringMaxLength } from 'buffer';
 import { Aluno } from './aluno';
 
 export class AlunoService {
   alunos: Aluno[] = [];
   
   gravar(aluno: Aluno): Aluno {
-    var result = null;
+    var result:any=null
     if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
